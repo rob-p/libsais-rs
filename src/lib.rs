285e2bb3f1858423fd42a8763fe6ba3db6923002
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-10-02 23:18:50 +0200

    Inline libsais-sys crate into libsais-rs::sys module

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,13 @@
 
 /// Direct bindings to the C API.
 pub mod sys {
-    pub use libsais_sys::*;
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(improper_ctypes)]
+    #![allow(deref_nullptr)]
+
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
 pub fn suffix_array_u8(text: &[u8], sa: &mut [i32]) -> Result<(), ()> {
